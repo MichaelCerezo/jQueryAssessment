46013149eb08381cfe4b48f555186959d8cbf3d0
Michael Cerezo <cerezo.michael.mc@gmail.com>
2015-04-20 12:26:31 -0700

    assessment 4-20-15

---
+++ main.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+ 
+    $( "div" ).click(function( dan ) {
+ 
+        alert( "Hey you clicked on me" );
+ 
+    });
+ 
+});
+$( document ).ready(function() {
+ 
+    $( "div" ).click(function( iwao ) {
+ 
+        alert( "Hey you clicked on me again" );
+ 
+    });
+ 
+});
